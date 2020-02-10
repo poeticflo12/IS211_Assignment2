91f1b8c6588d5858da205da56b146705263ca478
poeticflo12 <gelaneymatthew@gmail.com>
2020-02-10 17:33:16 -0500

    Second Commit

--- assignment2.py
+++ assignment2.py
@@ -9,15 +9,24 @@ import argparse
 
 # This reads and downloads the data from url that was given
 def downloadData(url):
-    url = urllib.request.urlopen(url)
-    return url
+    data = urllib.request.urlopen(url)
+    return data
 
 
 logging.basicConfig(filename="errors.log", level=logging.ERROR)
-logger = logging.getlogger("assignment2")
+logger = logging.getLogger("assignment2_IS211")
 
 
 # Processes datafile in CSV format
-def processData(file_data):
-    showfile = csv.dictreader(file_data)
-    newdict = {}
+def processData(data):
+    show = csv.DictReader(data)
+    dictionary_new = {}
+
+    for number, lines in enumerate(show):
+        born = datetime.datetime.strptime(lines["birthday"], "%d/%m%y")
+        dictionary_new[lines["identification"]] = (lines["name"], born)
+
+        logging.error(
+            "\nIssues Processing Line {} for ID# {}\n" .format(number, lines["identification"]))
+
+        return dictionary_new
